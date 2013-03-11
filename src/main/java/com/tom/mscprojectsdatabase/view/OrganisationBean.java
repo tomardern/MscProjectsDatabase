b8ca6fb21e62e72123b8980905f786150c95ecb6
TOM <TOM@192.168.11.4>
2013-03-11 16:50:11 +0000

    Added Panel and Improved Flow

--- src/main/java/com/tom/mscprojectsdatabase/view/OrganisationBean.java
+++ src/main/java/com/tom/mscprojectsdatabase/view/OrganisationBean.java
@@ -127,7 +127,7 @@ public class OrganisationBean implements Serializable
          else
          {
             this.entityManager.merge(this.organisation);
-            return "view?faces-redirect=true&id=" + this.organisation.getId();
+            return "panel?faces-redirect=true&id=" + this.organisation.getId();
          }
       }
       catch (Exception e)
