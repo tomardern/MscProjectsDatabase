742b7f7cab82116c6557d4d825c5f44a49196fa4
TOM <TOM@192.168.11.4>
2013-03-18 13:43:31 +0000

    Removed Delete from Panel, added Mailto and removed JS defaults.

--- src/main/webapp/resources/js/site.js
+++ src/main/webapp/resources/js/site.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
     
     
-    /* Random Text Generator */
+    /* Random Text Generator
     alpha = "abcdefghijklmnopqrstuvwxyz";
     randText = "";
    
     for(i=0; i<10; i++) {
         num = Math.floor(Math.random()*alpha.length);
         randText += alpha[num];
-    }
-    
-    $(".auto input[type='text'], .auto input[type='password'], textarea").each(function(){
+    } 
+     $(".auto input[type='text'], .auto input[type='password'], textarea").each(function(){
         if ($(this).val().length < 1){
             $(this).val(randText);
         }
-    });
+    }); */
     
     //HTML5 Date selector and today's date by default http://stackoverflow.com/questions/6982692/html5-input-type-date-default-value-to-today
     $(".date").attr("type","date").val(new Date().toJSON().substring(0,10));
